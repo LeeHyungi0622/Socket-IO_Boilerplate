662d191e3f3a936952a786b66642fde8513db110
Lee Hyungi <michael.hyungi.lee@gmail.com>
2025-02-16 16:18:13 +0900

    feat: Draw painting Socket.IO samples

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,6 @@ import { createServer } from 'http'
 import { Server } from 'socket.io'
 import * as express from 'express'
 import * as path from 'path'
-import RandomScreenNameGenerator from './randomScreenNameGenerator'
 
 const port = 3000
 
@@ -13,25 +12,9 @@ const server = createServer(app)
 
 const io = new Server(server)
 
-const randomScreenNameGenerator = new RandomScreenNameGenerator()
-
 io.on('connection', (socket) => {
-  console.log('a user connected : ' + socket.id)
-
-  let screenName = randomScreenNameGenerator.generateRandomScreenName()
-
-  socket.emit('screenName', screenName)
-
-  socket.broadcast.emit('systemMessage', screenName.name + ' has joined the chat')
-
-  socket.on('disconnect', () => {
-    console.log('socket disconnected : ' + socket.id)
-
-    socket.broadcast.emit('systemMessage', screenName.name + ' has left the chat')
-  })
-
-  socket.on('chatMessage', (message: ChatMessage) => {
-    socket.broadcast.emit('chatMessage', message)
+  socket.on('draw', (message) => {
+    socket.broadcast.emit('draw', message)
   })
 })
 
